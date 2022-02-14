8bd1292e2ba9568473308fad5cedf8d8a39d3cee
xNtrow <xntrowdev@gmail.com>
2022-02-14 10:49:47 +0100

    New Things

--- src/main/java/org/xnotro/playerinvisible/Main.java
+++ src/main/java/org/xnotro/playerinvisible/Main.java
@@ -1,9 +1,7 @@
 package org.xnotro.playerinvisible;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
